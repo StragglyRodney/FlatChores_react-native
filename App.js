185e7935e93b4e7e1de503e0a8a5ff041be2329b
Rodney Cruden-Powell <rodney.crudenpowell@gmail.com>
2018-09-28 22:19:26 +1200

    added very basic firebase authentication on login

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 import { createSwitchNavigator } from 'react-navigation'
 import { AuthStack, AppTab } from './src/lib/Navigators'
-import { firebase, firestore } from 'firebase'
 import { firebaseService } from './src/lib/FireBaseService'
+import firebase from 'firebase/app'
 
 firebaseService.load('test')
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyDhr9S4S4Gshpbb8WsRACyHimXOr0iczY8',
+  authDomain: 'flatchores-react-native.firebaseapp.com',
+  databaseURL: 'https://flatchores-react-native.firebaseio.com',
+  projectId: 'flatchores-react-native',
+  storageBucket: 'flatchores-react-native.appspot.com',
+  messagingSenderId: '415430905460'
+})
 
 export default createSwitchNavigator(
   {

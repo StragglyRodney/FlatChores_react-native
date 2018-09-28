a7b1941fd2fd35c054f6b8dc9bdf4e6d72687512
Rodney Cruden-Powell <rodney.crudenpowell@gmail.com>
2018-09-28 13:01:19 +1200

    firebase works with ios

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { createSwitchNavigator } from 'react-navigation'
 import { AuthStack, AppTab } from './src/lib/Navigators'
+import { firebase, firestore } from 'firebase'
+import { firebaseService } from './src/lib/FireBaseService'
 
 export default createSwitchNavigator(
   {
